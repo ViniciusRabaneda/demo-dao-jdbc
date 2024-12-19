58459a90d726fb04121479c7091984c196b51ccf
Vinicius <vinirabaneda@hotmail.com>
2024-12-19 11:44:29 -0300

    DepartmentDao implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //DaoFactory é uma forma de fazer a injeção de dependência sem explicitar a implementação
@@ -10,5 +11,9 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
-
+	
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
